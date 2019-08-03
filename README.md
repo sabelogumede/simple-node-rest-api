2926a226b531a3029dee26324c1923f417002a98
sabelogumede <iamsabelogumede@gmail.com>
2019-08-03 08:40:50 +0200

    initial project

---
+++ README.md
@@ -0,0 +1,2 @@
+#My simple rest api
+
---
+++ users/models/users.model.js
@@ -0,0 +1,7 @@
+const userSchema = new Schema({
+    firstName: String,
+    lastName: String,
+    email: String,
+    password: String,
+    permissionLevel: Number
+ });
